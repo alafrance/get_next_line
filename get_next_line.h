618dfeec4ff59209ed67aa6b83cc0e8e3a854055
alafranc <alafranc@student.42lyon.fr>
2020-12-01 15:44:16 +0100

    change header

--- get_next_line.h
+++ get_next_line.h
@@ -6,23 +6,20 @@
 /*   By: alafranc <alafranc@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 23:59:34 by alafranc          #+#    #+#             */
-/*   Updated: 2020/12/01 15:29:07 by alafranc         ###   ########lyon.fr   */
+/*   Updated: 2020/12/01 15:42:24 by alafranc         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-
-int		get_next_line(int fd, char **line);
-char	*ft_strjoin(char *s1, char *s2);
-size_t	ft_strlen(const char *s);
-char	*ft_strdup(char *src);
-void	*ft_strccpy(char *file, int c);
-int		ft_strchr(char *file, int c);
-char	*ft_substr_line(char *s);
 char	*ft_remove_first_line(char *file);
 char	*fill_filebuf(int fd, char *filebuf, int *b_read);
+int		get_next_line(int fd, char **line);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(char *src);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_strchr(char *file, int c);
+char	*ft_substr_line(char *s);
 #endif
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,23 +6,21 @@
 /*   By: alafranc <alafranc@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 23:59:34 by alafranc          #+#    #+#             */
-/*   Updated: 2020/12/01 15:22:37 by alafranc         ###   ########lyon.fr   */
+/*   Updated: 2020/12/01 15:44:01 by alafranc         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
-# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 
+char	*ft_remove_first_line(char *file);
+char	*fill_filebuf(int fd, char *filebuf, int *b_read, int *eof);
 int		get_next_line(int fd, char **line);
-char	*ft_strjoin(char *s1, char *s2);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(char *src);
-void	*ft_strccpy(char *file, int c);
+char	*ft_strjoin(char *s1, char *s2);
 int		ft_strchr(char *file, int c);
 char	*ft_substr_line(char *s);
-char	*fill_filebuf(int fd, char *filebuf, int *b_read, int *eof);
-char	*ft_remove_first_line(char *file);
 #endif
